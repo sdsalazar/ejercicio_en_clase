7cfffecf815457e40e1f88912c03fde3c38aef11
sandra salazar <sdsalazar@misena.edu.co>
2016-08-08 20:17:22 -0500

    Version inicial - Sandra y Stephany

---
+++ Hola_Mundo.py
@@ -0,0 +1,92 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print "Hola_Mundo"
+
+
+# $$f(x)=li(x) - \sum_{p}{li}(x^p)-log(2) + \int_\chi^\infty \frac{dt}{t(t^2-1)log(t)}$$
+
+# In[2]:
+
+2+4*6/8.0-3
+
+
+# In[3]:
+
+3/2
+
+
+# In[6]:
+
+(3+5j)-(1-1j)
+
+
+# # SANDRAAAA _:P
+
+# In[7]:
+
+5**3
+
+
+# In[12]:
+
+abs(3+5j)
+
+
+# In[14]:
+
+int(_12)
+
+
+# In[15]:
+
+float(_14)
+
+
+# In[20]:
+
+complex(_18)
+
+
+# In[21]:
+
+max(8,10,20,30)
+
+
+# In[22]:
+
+min(20,10,30,5,40)
+
+
+# In[23]:
+
+x,y,z,a = 0,1,2,3
+
+
+# In[27]:
+
+print(x); print(y); print(z); print(a) 
+
+
+# In[30]:
+
+x = a; y = z; z = x
+
+
+# In[31]:
+
+print(x); print(y); print(z); print(a)
+
+
+# In[42]:
+
+x = 5
+y = 8 
+if x > y :
+    print( "El mayor es" , y )
+else:
+    print("El menor es" , x)
+    
+
